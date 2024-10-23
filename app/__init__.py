0e8ac9f5fe89af249cde71f28bdea9ae0cbcdfd6
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2024-10-23 22:52:30 +0200

    feat: fastapi main entrypoint

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+
+from fastapi import Depends, FastAPI
+from fastapi.responses import ORJSONResponse
+
+from app.core.settings import get_settings
+from app.health.routers import router as health_router
+from app.status.routers import router as status_router
+
+settings = get_settings()
+
+logging.basicConfig(
+    stream=sys.stdout, level=logging.DEBUG if settings.debug else logging.INFO
+)
+logging.getLogger("hishel.controller").setLevel(settings.debug_level)
+
+app = FastAPI(
+    debug=settings.debug,
+    title=settings.title,
+    version=settings.version,
+    dependencies=[Depends(get_settings)],
+    default_response_class=ORJSONResponse,
+)
+
+app.include_router(health_router)
+app.include_router(status_router, prefix="/status")
