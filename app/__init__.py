19c819725239654507921fe5d9449dc2151f705a
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2024-10-29 23:17:37 +0100

    feat: app instance

--- app/__init__.py
+++ app/__init__.py
@@ -6,16 +6,22 @@ from app.devices.routers import router as devices_router
 from app.health.routers import router as health_router
 from app.router.routers import router as router_router
 
-settings = get_settings()
 
-app = FastAPI(
-    debug=settings.debug,
-    title="SpeedPort API",
-    version=settings.version,
-    dependencies=[Depends(get_settings)],
-    default_response_class=ORJSONResponse,
-)
+def get_application() -> FastAPI:
+    settings = get_settings()
+    app = FastAPI(
+        debug=settings.debug,
+        title="SpeedPort API",
+        version=settings.version,
+        dependencies=[Depends(get_settings)],
+        default_response_class=ORJSONResponse,
+    )
 
-app.include_router(health_router)
-app.include_router(router_router, prefix="/router")
-app.include_router(devices_router, prefix="/router")
+    app.include_router(health_router)
+    app.include_router(router_router, prefix="/router")
+    app.include_router(devices_router, prefix="/router")
+
+    return app
+
+
+app = get_application()
